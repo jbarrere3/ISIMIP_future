4cbe53742b0be643e6cd79e0427a0273e479d0b1
jbarrere3 <julienbarrere3@gmail.com>
2023-10-10 17:01:11 +0200

    Finished to implement download of future data

--- dl_allclim.sh
+++ dl_allclim.sh
@@ -2,6 +2,9 @@
 
 # Arguments : 1 = csv file with all variables, 2 = model for which to extract data, 3 = csv file with future time periods
 
+# Inform the log file
+echo "Time: $(date). Begin script" >> dlclim.log
+
 # Create directory for the model if it doesn't exist
 if [ ! -d ${2} ]
   then mkdir ${2}
@@ -13,10 +16,15 @@ while read variables; do
       then bash dl_historical.sh $variables $2 &
     fi
 done < "$1"
+echo "Time: $(date). (model $2) - End download of historical data" >> dlclim.log
 
-# Loop on all variables to download future data from model $2, ssp126
+# Loop on all time periods to download future data from model $2, ssp126
 while read timeperiod; do
-    if [ ! -f $2/${variables}_2011_2014.nc ]
-      then bash dl_historical.sh $variables $2 &
-    fi
+    # Loop on all variables to download data from model $2 (historical data)
+    while read var; do
+        if [ ! -f $2/${var}_${timeperiod}_ssp126.nc ]
+          then bash dl_climssptime.sh $var $2 ssp126 $timeperiod &
+        fi
+        echo "Time: $(date). (model $2) - End download of ssp126, $timeperiod" >> dlclim.log
+    done < "$1"
 done < "$3"
