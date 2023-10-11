88403bd476078583cab8067196f846c1b654e675
jbarrere3 <julienbarrere3@gmail.com>
2023-10-11 19:04:54 +0200

    bugfix14

--- get_pet.sh
+++ get_pet.sh
@@ -35,9 +35,10 @@ cdo subc,273.15 $input_file_tas "${st}tas.nc"
 # Calculate vapor pressure deficit
 cdo expr,'esTmax = (0.6108*exp((17.27*tasmax)/(tasmax+237.3)))' "${st}tasmax.nc" "${st}esTmax.nc"
 cdo expr,'esTmin = (0.6108*exp((17.27*tasmin)/(tasmin+237.3)))' "${st}tasmin.nc" "${st}esTmin.nc"
+cdo -divc,2 -add "${st}esTmax.nc" "${st}esTmin.nc" "${st}es.nc"
+
 #cdo expr,'es = (esTmax + esTmin)/2' -selvar,esTmax "${st}esTmax.nc" -selvar,esTmin "${st}esTmin.nc" "${st}es.nc"
-cdo -selvar,esTmax "${st}esTmax.nc" -selvar,esTmin "${st}esTmin.nc" expr,'es = (esTmax + esTmin)/2' "${st}es.nc"
-cdo -selvar,es "${st}es.nc" -selvar,hurs $input_file_hurs expr,'vpd = es*(1 - hurs/100)' "${st}vpd.nc"
+#cdo -selvar,es "${st}es.nc" -selvar,hurs $input_file_hurs expr,'vpd = es*(1 - hurs/100)' "${st}vpd.nc"
 # cdo expr,'Delta = (4098*0.6108*exp((17.27*(tas-273.15))/((tas-273.15)+237.3)))/(((tas-273.15)+237.3)^2)' $input_file_tas $2_$1_Delta.nc
 # cdo expr,'Pr = (4098*0.6108*exp((17.27*(tas-273.15))/((tas-273.15)+237.3)))/(((tas-273.15)+237.3)^2)' $input_file_tas $2_$1_Delta.nc
 
@@ -48,13 +49,13 @@ for ((year = year1; year <= year2; year++)); do
     # output_file_sgdd="sgdd_$year.nc"
     # output_file_wai="wai_$year.nc"
     output_file_tmean="tmean_${year}.nc"
-    output_file_vpdmean="vpdmean_${year}.nc"
+    output_file_esmean="esmean_${year}.nc"
 
     # Use CDO to calculate sgdd and wai for the current year
     cdo yearmean -selyear,${year} "${st}tas.nc" $output_file_tmean
-    cdo yearmean -selyear,${year} "${st}vpd.nc" $output_file_vpdmean
+    cdo yearmean -selyear,${year} "${st}es.nc" $output_file_esmean
 
-    echo "Generated $output_file_tmean and $output_file_vpdmean"
+    echo "Generated $output_file_tmean and $output_file_esmean"
 done
 
 echo "All annual files generated in the current directory"
