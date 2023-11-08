6121f941a553e66f31cfad4a7bdbed9f231126c6
jbarrere3 <julienbarrere3@gmail.com>
2023-11-08 11:18:05 +0100

    bug fix

--- bashscripts/dl_climssptime.sh
+++ bashscripts/dl_climssptime.sh
@@ -5,8 +5,5 @@
 # Download global file
 wget https://files.isimip.org/ISIMIP3b/InputData/climate/atmosphere/bias-adjusted/global/daily/$3/${2^^}/$2_r1i1p1f1_w5e5_$3_$1_global_daily_$4.nc
 
-# Crop the file to European boundaries 
-cdo -f nc4c -z zip_5 -masklonlatbox,-12,42,34,73 $2_r1i1p1f1_w5e5_$3_$1_global_daily_$4.nc $2/data/$1_$4_$3.nc
-
-# Remove the global file
-rm $2_r1i1p1f1_w5e5_$3_$1_global_daily_$4.nc
+# Move the file extracted
+mv ${2}_r1i1p1f1_w5e5_${3}_${1}_global_daily_$4.nc $2/data/$1_$4_$3.nc
